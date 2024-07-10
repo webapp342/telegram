66f48c90e528c8a1da82ce6dfbc470799532e579
19984646 <busetaktas45@gmail.com>
2024-07-10 04:32:51 +0330

    Refactor: Description of your changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import "./App.css";
 import { TonConnectButton } from "@tonconnect/ui-react";
 import { Counter } from "./components/Counter";
@@ -8,6 +9,7 @@ import { Button, FlexBoxCol, FlexBoxRow } from "./components/styled/styled";
 import { useTonConnect } from "./hooks/useTonConnect";
 import { CHAIN } from "@tonconnect/protocol";
 import "@twa-dev/sdk";
+import TelegramUsername from "./components/TelegramUsername";
 
 const StyledApp = styled.div`
   background-color: #e8e8e8;
@@ -43,6 +45,7 @@ function App() {
                 : "N/A"}
             </Button>
           </FlexBoxRow>
+          <TelegramUsername />
           <Counter />
           <TransferTon />
           <Jetton />
