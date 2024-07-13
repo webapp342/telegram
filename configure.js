ae44b293753ba6ccf8e2939855c3adfc7002b465
19984646 <busetaktas45@gmail.com>
2024-07-14 02:14:15 +0330

    first commit

--- configure.js
+++ configure.js
@@ -70,7 +70,7 @@ let githubUsername, githubRepo, botUsername;
     {
       menu_button: {
         type: "web_app",
-        text: "Launch Webapp",
+        text: "Launch",
         web_app: {
           url: url,
         },
