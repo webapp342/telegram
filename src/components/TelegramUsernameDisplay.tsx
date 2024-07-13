d5764f18caee10cb6fadc1b695a02f3c482204cf
19984646 <busetaktas45@gmail.com>
2024-07-14 01:43:42 +0330

    first commit

--- src/components/TelegramUsernameDisplay.tsx
+++ src/components/TelegramUsernameDisplay.tsx
@@ -1,8 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import WebApp from '@twa-dev/sdk';
-import app from './firebaseConfig'; // Yapılandırmayı buradan içe aktar
+import { initializeApp } from "firebase/app";
 import { getFirestore, setDoc, doc } from "firebase/firestore";
 
+// Firebase yapılandırması
+const firebaseConfig = {
+  apiKey: "AIzaSyBQwjgfS2FZWZJtGJPFAJNS58YcAdUVDeE",
+  authDomain: "telegram-cc828.firebaseapp.com",
+  projectId: "telegram-cc828",
+  storageBucket: "telegram-cc828.appspot.com",
+  messagingSenderId: "541310609374",
+  appId: "1:541310609374:web:5c046692c1ee59ba981f4f",
+  measurementId: "G-7XGPVD7NY4"
+};
+
+// Firebase'i başlat
+const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 
 const TelegramUsernameDisplay: React.FC = () => {
