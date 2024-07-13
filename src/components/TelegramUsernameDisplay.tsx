ef6bd92b861902d2b663649bb2dc8232cd08fa3a
19984646 <busetaktas45@gmail.com>
2024-07-13 05:24:32 +0330

    first commit

--- src/components/TelegramUsernameDisplay.tsx
+++ src/components/TelegramUsernameDisplay.tsx
@@ -1,38 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import WebApp from '@twa-dev/sdk';
+import { initializeApp } from "firebase/app";
+import { getFirestore, setDoc, doc } from "firebase/firestore";
+
+// Firebase yapılandırması
+const firebaseConfig = {
+  apiKey: "AIzaSyBQwjgfS2FZWZJtGJPFAJNS58YcAdUVDeE",
+  authDomain: "telegram-cc828.firebaseapp.com",
+  projectId: "telegram-cc828",
+  storageBucket: "telegram-cc828.appspot.com",
+  messagingSenderId: "541310609374",
+  appId: "1:541310609374:web:5c046692c1ee59ba981f4f",
+  measurementId: "G-7XGPVD7NY4"
+};
+
+// Firebase'i başlat
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
 
 const TelegramUsernameDisplay: React.FC = () => {
   const [telegramUsername, setTelegramUsername] = useState<string | null>(null);
 
   useEffect(() => {
-    // Telegram Web App initialization
     const initData = WebApp.initDataUnsafe;
 
-    // Kullanıcı adı mevcutsa state'e kaydediyoruz
     if (initData && initData.user && initData.user.username) {
-      setTelegramUsername(initData.user.username);
-      // Kullanıcı adını backend'e gönder
-      fetch('https://vercel-wheat-one.vercel.app/api/users', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username: initData.user.username }),
+      const username = initData.user.username;
+      setTelegramUsername(username);
+
+      // Kullanıcı adını Firestore'a kaydet
+      setDoc(doc(firestore, 'users', username), {
+        username: username,
       })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Success:', data);
+      .then(() => {
+        console.log('Kullanıcı adı Firestore\'a kaydedildi:', username);
       })
       .catch((error) => {
-        console.error('Error:', error);
+        console.error('Firestore hatası:', error);
       });
     } else {
       console.error('Kullanıcı bilgileri alınamadı veya kullanıcı adı mevcut değil');
-      setTelegramUsername(null); // Açıkça null olarak ayarlıyoruz
+      setTelegramUsername(null);
     }
   }, []);
 
-  // Davet linki oluşturma fonksiyonu
   const generateInviteLink = () => {
     if (telegramUsername) {
       return `https://t.me/kastamonmubot?start=${telegramUsername}`;
