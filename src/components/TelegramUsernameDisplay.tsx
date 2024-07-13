0b429d25a368d56454fa09665edc0c72e2667882
19984646 <busetaktas45@gmail.com>
2024-07-13 06:07:36 +0330

    first commit

--- src/components/TelegramUsernameDisplay.tsx
+++ src/components/TelegramUsernameDisplay.tsx
@@ -7,7 +7,6 @@ const firestore = getFirestore(app);
 
 const TelegramUsernameDisplay: React.FC = () => {
   const [telegramUsername, setTelegramUsername] = useState<string | null>(null);
-  const [inviterUsername, setInviterUsername] = useState<string | null>(null);
 
   useEffect(() => {
     const initData = WebApp.initDataUnsafe;
@@ -16,23 +15,25 @@ const TelegramUsernameDisplay: React.FC = () => {
       const username = initData.user.username;
       setTelegramUsername(username);
 
-      // Kullanıcı adını Firestore'a kaydet
-      setDoc(doc(firestore, 'users', username), {
-        username: username,
-        points: 0, // Başlangıçta 0 puan
-      })
-      .then(() => {
-        console.log('Kullanıcı adı Firestore\'a kaydedildi:', username);
-      })
-      .catch((error) => {
-        console.error('Firestore hatası:', error);
+      const userDoc = doc(firestore, 'users', username);
+      getDoc(userDoc).then(userSnapshot => {
+        if (!userSnapshot.exists()) {
+          setDoc(userDoc, {
+            username: username,
+            points: 0,
+          })
+          .then(() => {
+            console.log('Kullanıcı adı Firestore\'a kaydedildi:', username);
+          })
+          .catch((error) => {
+            console.error('Firestore hatası:', error);
+          });
+        }
       });
 
-      // Davet eden kullanıcı adı kontrolü
       const urlParams = new URLSearchParams(window.location.search);
       const inviter = urlParams.get('start');
       if (inviter) {
-        setInviterUsername(inviter);
         updateInviterPoints(inviter);
       }
     } else {
