ea5b41c5c38558a5cb58515276bd0af6965036e0
19984646 <busetaktas45@gmail.com>
2024-07-13 05:36:18 +0330

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { TransferTon } from "./components/TransferTon";
 import { Button, FlexBoxCol, FlexBoxRow } from "./components/styled/styled";
 import { useTonConnect } from "./hooks/useTonConnect";
 import { CHAIN } from "@tonconnect/protocol";
+import TelegramPoints from './components/TelegramPoints';
 
 const StyledApp = styled.div`
   background-color: #e8e8e8;
@@ -45,6 +46,8 @@ function App() {
             </Button>
           </FlexBoxRow>
           <TelegramUsernameDisplay />
+          <TelegramPoints />
+
 
           <Counter />
           <TransferTon />
