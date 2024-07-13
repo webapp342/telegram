8c949c976ea0f25971e8e79f5ac2e49ad8c46584
19984646 <busetaktas45@gmail.com>
2024-07-14 02:08:32 +0330

    first commit

--- src/components/TelegramUsernameDisplay.tsx
+++ src/components/TelegramUsernameDisplay.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import WebApp from '@twa-dev/sdk';
-import app from './firebaseConfig'; // Yapılandırmayı buradan içe aktar
+import app from './firebaseConfig';
 import { getFirestore, setDoc, doc, getDoc } from "firebase/firestore";
 
 const firestore = getFirestore(app);
 
 const TelegramUsernameDisplay: React.FC = () => {
   const [telegramUsername, setTelegramUsername] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const initData = WebApp.initDataUnsafe;
@@ -17,42 +19,45 @@ const TelegramUsernameDisplay: React.FC = () => {
 
       const userDocRef = doc(firestore, 'users', username);
 
-      // Kullanıcı adını Firestore'a kaydetmeden önce kontrol et
       getDoc(userDocRef).then((docSnapshot) => {
         if (docSnapshot.exists()) {
           console.log('Kullanıcı zaten kayıtlı:', username);
         } else {
-          // Kullanıcı adı Firestore'a kaydet
           setDoc(userDocRef, {
             username: username,
-            puan: 0,  // Yeni kullanıcı için başlangıç puanı
+            puan: 0,
           })
           .then(() => {
             console.log('Kullanıcı adı Firestore\'a kaydedildi:', username);
           })
           .catch((error) => {
             console.error('Firestore hatası:', error);
+            setError('Firestore hatası: ' + error.message);
           });
         }
       }).catch((error) => {
         console.error('Kullanıcı bilgileri alınırken hata oluştu:', error);
+        setError('Kullanıcı bilgileri alınırken hata oluştu: ' + error.message);
       });
     } else {
       console.error('Kullanıcı bilgileri alınamadı veya kullanıcı adı mevcut değil');
       setTelegramUsername(null);
+      setError('Kullanıcı adı bulunamadı.');
     }
+    setLoading(false);
   }, []);
 
   const generateInviteLink = () => {
-    if (telegramUsername) {
-      return `https://t.me/kastamonmubot?start=${telegramUsername}`;
-    }
-    return 'https://t.me/kastamonmubot';
+    return telegramUsername ? `https://t.me/kastamonmubot?start=${telegramUsername}` : 'https://t.me/kastamonmubot';
   };
+
+  if (loading) return <p>Yükleniyor...</p>;
 
   return (
     <div>
-      {telegramUsername ? (
+      {error ? (
+        <p style={{ color: 'red' }}>{error}</p>
+      ) : telegramUsername ? (
         <>
           <p>Telegram Kullanıcı Adı: {telegramUsername}</p>
           <p>
