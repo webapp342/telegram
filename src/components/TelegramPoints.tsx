a6f9ceb6e58be4b122a6f4b34abd265d1c4287c6
19984646 <busetaktas45@gmail.com>
2024-07-13 05:50:18 +0330

    first commit

--- src/components/TelegramPoints.tsx
+++ src/components/TelegramPoints.tsx
@@ -1,19 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import WebApp from '@twa-dev/sdk';
-import { initializeApp } from "firebase/app";
+import app from './firebaseConfig';
 import { getFirestore, doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
 
-// Firebase yapılandırması
-const firebaseConfig = {
-  apiKey: "AIzaSyBQwjgfS2FZWZJtGJPFAJNS58YcAdUVDeE",
-  authDomain: "telegram-cc828.firebaseapp.com",
-  projectId: "telegram-cc828",
-  storageBucket: "telegram-cc828.appspot.com",
-  messagingSenderId: "541310609374",
-  appId: "1:541310609374:web:5c046692c1ee59ba981f4f",
-};
-
-const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 
 const TelegramPoints: React.FC = () => {
@@ -22,7 +11,7 @@ const TelegramPoints: React.FC = () => {
 
   useEffect(() => {
     const initData = WebApp.initDataUnsafe;
-    
+
     if (initData && initData.user && initData.user.username) {
       const username = initData.user.username;
       setTelegramUsername(username);
---
+++ src/components/firebaseConfig.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQwjgfS2FZWZJtGJPFAJNS58YcAdUVDeE",
+  authDomain: "telegram-cc828.firebaseapp.com",
+  projectId: "telegram-cc828",
+  storageBucket: "telegram-cc828.appspot.com",
+  messagingSenderId: "541310609374",
+  appId: "1:541310609374:web:5c046692c1ee59ba981f4f",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
